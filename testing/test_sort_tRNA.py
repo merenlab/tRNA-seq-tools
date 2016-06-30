8442e931590baaa2557c5b6a936fb05da2376336
Steven Cui <scui@minnie.bpcservers.private>
2016-06-30 18:37:40 -0400

    added testing directory

---
+++ testing/test_sort_tRNA.py
@@ -0,0 +1,15 @@
+import unittest
+import sys
+
+sys.path.append("../")
+
+import sort_tRNA as sort
+
+
+class SortTestCase(unittest.TestCase):
+    
+    def test_
+
+
+if __name__ == "__main__":
+    unittest.main()
