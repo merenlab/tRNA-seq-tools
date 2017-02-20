d4ee534d73a0a1dfae77b1cd15e3c87a3621f22d
A. Murat Eren <a.murat.eren@gmail.com>
2017-02-20 09:53:54 -0600

    no need to have a stats table structure

--- tRNASeqTools/tables.py
+++ tRNASeqTools/tables.py
@@ -2,15 +2,17 @@
 # pylint: disable=line-too-long
 """table schemas for database"""
 
+
 __author__ = "Steven Cui"
-__copyright__ = "Copyright 2016, The University of Chicago"
+__copyright__ = "Copyright 2017, Meren Lab"
 __credits__ = []
 __license__ = "GPL 3.0"
-__version__ = 0.1
-__maintainer__ = "Steven Cui"
-__email__ = "stevencui729@gmail.com"
+__maintainer__ = "A. Murat Eren"
+__email__ = "a.murat.erengmail.com"
 
-__profile_db__version__ = 1
+
+profile_db_version = 1
+
 
 ###############################################################################
 #
@@ -23,13 +25,3 @@ __profile_db__version__ = 1
 profile_table_name      = "profile"
 profile_table_structure = ["ID",   "Seq" , "Three_trailer", "T_loop", "Acceptor", "Full_length", "Seq_length", "Trailer_length", "Anticodon"]
 profile_table_types     = ["text", "text",     "text"     ,  "text" ,   "text"  ,     "text"   ,    "int"    ,      "int"      ,   "text"   ]
-
-# table schemas for stats table
-stats_table_name = "stats"
-stats_table_structure = ["Total_seqs", "Total_full_length", "With_trailer",
-    "Total_passed", "No_divergence", "T_loop_divergence", "Pos_0_divergence",
-    "Pos_1_divergence", "Pos_2_divergence", "Pos_3_divergence", 
-    "Pos_8_divergence", "Acceptor_divergence", "Pos_neg_3_divergence", 
-    "Pos_neg_2_divergence", "Pos_neg_1_divergence", "Total_failed", "T_loop_reject",
-    "Acceptor_reject", "Both_reject", "Short_reject"]
-stats_table_types = ["int"] * len(stats_table_structure)
