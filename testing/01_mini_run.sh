6c5b691b611cdadbe10032c3d7b6442d43609365
A. Murat Eren <a.murat.eren@gmail.com>
2017-02-20 09:54:14 -0600

    example stats

--- testing/01_mini_run.sh
+++ testing/01_mini_run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 source 00.sh
 
+set -e
+
 # Setup #############################
 SETUP_WITH_OUTPUT_DIR $1
 #####################################
 
 INFO "Profiling raw tRNA sequences"
 trna-profile $files/raw_tRNA_sequences.fa -o $output_dir/test_tRNA_profile.db -s test_sample
+
+INFO "Printing stats"
+trna-get-db-info -p $output_dir/test_tRNA_profile.db
