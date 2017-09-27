5364e99d81c2725c3c902d7b7532d64bd3f17e9a
A. Murat Eren <a.murat.eren@gmail.com>
2017-09-27 17:47:48 -0500

    report FASTA can deal with uniques

--- tRNASeqTools/utils.py
+++ tRNASeqTools/utils.py
@@ -72,13 +72,26 @@ def store_dict_as_TAB_delimited_file(d, output_path, headers=None, file_obj=None
     return output_path
 
 
-def store_dict_as_FASTA_file(d, output_file_path, wrap_from=200):
+def store_dict_as_FASTA_file(d, output_file_path, report_unique_sequences=False, wrap_from=200):
     filesnpaths.is_output_file_writable(output_file_path)
     output = open(output_file_path, 'w')
 
-    for key in d:
-        output.write('>%s\n' % key)
-        output.write('%s\n' % textwrap.fill(d[key], wrap_from, break_on_hyphens=False))
+    props_h = sorted(list(list(d.values())[0]['props'].keys()))
+
+    if report_unique_sequences:
+        seqs_sorted_by_frequency = [x[1] for x in sorted([(len(d[seq]['ids']), seq) for seq in d], reverse=True)]
+
+        for seq in seqs_sorted_by_frequency:
+            frequency = len(d[seq]['ids'])
+            seq_id = d[seq]['ids'].pop()
+            output.write('>%s %s|frequency:%d\n' % (seq_id, '|'.join(['%s:%s' % (t[0], t[1]) for t in d[seq]['props'].items()]), frequency))
+            output.write('%s\n' % textwrap.fill(seq, wrap_from, break_on_hyphens=False))
+    else:
+        for seq in d:
+            props = '|'.join(['%s:%s' % (t[0], t[1]) for t in d[seq]['props'].items()])
+            for seq_id in d[seq]['ids']:
+                output.write('>%s %s\n' % (seq_id, props))
+                output.write('%s\n' % textwrap.fill(seq, wrap_from, break_on_hyphens=False))
 
     output.close()
     return True
